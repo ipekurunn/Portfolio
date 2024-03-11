2312561dbdf981b50fea2bfcd09838f64d013dec
İpek Ürün <ipekurun.96@hotmail.com>
2024-03-11 22:04:59 +0300

    incorrect use of label name is fixed

--- js/script.js
+++ js/script.js
@@ -77,3 +77,16 @@ window.onload = function () {
       );
     });
 };
+
+
+$(".meter > span").each(function () {
+  $(this)
+    .data("origWidth", $(this).width())
+    .width(0)
+    .animate(
+      {
+        width: $(this).data("origWidth")
+      },
+      1200
+    );
+});
